7e4fa72aae5dd1d9f11f364db22de912e3797d14
netstermonir <monirhossain6163@gmail.com>
2021-08-29 15:30:36 +0600

    ultapalta conditation check

--- js/sports-api.js
+++ js/sports-api.js
@@ -1,21 +1,50 @@
-const loadSports = () => {
+document.getElementById('not-found').style.display = 'none';
+const loadSports = async () => {
     const searchField = document.getElementById('search-team');
     const searchResult = searchField.value;
     searchField.value = "";
+    document.getElementById('not-found').style.display = 'none';
     if(searchResult == ""){
-        alert('Please Enter Your FootBall Team Name');
+        const message = document.getElementById('erorr-message');
+        message.innerHTML = `<div class=" w-100 mx-auto alert alert-danger alert-dismissible fade show" role="alert">
+  <strong>Hello 3rd Person!</strong> Please Enter Your FuckUp Name.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>`;
     }
     else{
         const url = `https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${searchResult}`;
-        fetch(url)
-        .then(res => res.json())
-        .then(data => displayTeam(data))
+        // fetch(url)
+        // .then(res => res.json())
+        // .then(data => displayTeam(data))
+        // .catch(error => displayerror(error));
+        try{
+            const res = await fetch(url);
+            const data = await res.json();
+            displayTeam(data);
+        }
+        catch{
+            document.getElementById('not-found').style.display = 'block';
+            const message = document.getElementById('not-found');
+            message.innerHTML = `<div class=" w-100 mx-auto alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>Hello 3rd Person!</strong> Search Result Team Not Found.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>`;
+        }
     }
     
 }
+// const displayerror = error => {
+//     document.getElementById('not-found').style.display = 'block';
+//     const message = document.getElementById('not-found');
+//        message.innerHTML = `<div class=" w-100 mx-auto alert alert-danger alert-dismissible fade show" role="alert">
+//   <strong>Hello 3rd Person!</strong> Search Result Product Not Found.
+//   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+// </div>`;
+// }
 const displayTeam = teams => {
     const showTeam = teams.teams;
     const teamElement = document.getElementById('sports-team');
+    teamElement.textContent = "";
     for(const teams of showTeam){
         const div = document.createElement('div');
         div.classList.add('col-md-4');
